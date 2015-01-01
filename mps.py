f0d4b43315abb97d7a73997dac186dfad26bde2e
Daniel Jones <tortxof@gmail.com>
2015-01-01 15:41:27 -0500

    Added MealplanDatabase class.

--- mps.py
+++ mps.py
@@ -15,6 +15,40 @@ def load_templates(template_dir):
 
 html = load_templates('templates')
 
+class MealplanDatabase(object):
+    def __init__(self):
+        self.dbfile = 'mps.db'
+
+    def new_db(self):
+        conn = sqlite3.connect(self.dbfile)
+        conn.execute('create virtual table mealplans using fts4(plan_type, date, content, notindexed=date)')
+        conn.commit()
+        conn.close()
+
+    def add(self, plan_type, date, content):
+        conn = sqlite3.connect(self.dbfile)
+        conn.execute('insert into mealplans values(?, ?, ?)', (plan_type, date, content))
+        conn.commit()
+        conn.close()
+
+    def remove(self, rowid):
+        conn = sqlite3.connect(self.dbfile)
+        conn.execute('delete from mealplans where rowid=?', (rowid,))
+        conn.commit()
+        conn.close()
+
+    def get(self, rowid):
+        conn = sqlite3.connect(self.dbfile)
+        record = conn.execute('select *,rowid from mealplans where rowid=?', (rowid,)).fetchone()
+        conn.close()
+        return record
+
+    def search(self, query):
+        conn = sqlite3.connect(self.dbfile)
+        records = conn.execute('select *,rowid from mealplans where mealplans match ?', (query,)).fetchall()
+        conn.close()
+        return records
+
 class Root(object):
     @cherrypy.expose
     def index(self):
