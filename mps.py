8651e14fae67443b98fe3e55cf6532fd7f04bc41
Daniel Jones <tortxof@gmail.com>
2015-01-01 23:16:30 -0500

    Added filename field and pdf text extraction.

--- mps.py
+++ mps.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import os
+import subprocess
 import sqlite3
 
 import cherrypy
@@ -13,22 +14,25 @@ def load_templates(template_dir):
             html[template] = f.read()
     return html
 
+def pdftotext(data):
+    return subprocess.check_output(['pdftotext', '-', '-'], input=data).decode()
+
 html = load_templates('templates')
 
 class MealplanDatabase(object):
     def __init__(self):
         self.dbfile = 'mps.db'
-        self.fields = ('plan_type', 'date', 'content', 'rowid')
+        self.fields = ('plan_type', 'date', 'filename', 'content', 'rowid')
 
     def new_db(self):
         conn = sqlite3.connect(self.dbfile)
-        conn.execute('create virtual table mealplans using fts4(plan_type, date, content, notindexed=date)')
+        conn.execute('create virtual table mealplans using fts4(plan_type, date, filename, content, notindexed=date)')
         conn.commit()
         conn.close()
 
-    def add(self, plan_type, date, content):
+    def add(self, plan_type, date, filename, content):
         conn = sqlite3.connect(self.dbfile)
-        conn.execute('insert into mealplans values(?, ?, ?)', (plan_type, date, content))
+        conn.execute('insert into mealplans values(?, ?, ?, ?)', (plan_type, date, filename, content))
         conn.commit()
         conn.close()
 
@@ -74,8 +78,10 @@ class Root(object):
     def add(self, pdf_file=None, plan_type=None, date=None):
         out = ''
         if pdf_file and plan_type and date:
-            content = pdf_file.file.read()
-            mealplan_db.add(plan_type, date, content)
+            filename = pdf_file.filename
+            content = pdftotext(pdf_file.file.read())
+            content = ' '.join(content.split())
+            mealplan_db.add(plan_type, date, filename, content)
         else:
             out += html['add']
         return html['template'].format(content=out)
